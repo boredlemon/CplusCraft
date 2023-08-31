6fdc2e89add6274e88af157f092cd079f11f858b
Dolphin-glitch <114450467+Dolphin-glitch@users.noreply.github.com>
2023-08-30 22:30:48 -0400

    first commit

---
+++ lib/cglm/CMakeFiles/cglm.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/affine.c" "CMakeFiles/cglm.dir/src/affine.c.o" "gcc" "CMakeFiles/cglm.dir/src/affine.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/affine2d.c" "CMakeFiles/cglm.dir/src/affine2d.c.o" "gcc" "CMakeFiles/cglm.dir/src/affine2d.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/bezier.c" "CMakeFiles/cglm.dir/src/bezier.c.o" "gcc" "CMakeFiles/cglm.dir/src/bezier.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/box.c" "CMakeFiles/cglm.dir/src/box.c.o" "gcc" "CMakeFiles/cglm.dir/src/box.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/cam.c" "CMakeFiles/cglm.dir/src/cam.c.o" "gcc" "CMakeFiles/cglm.dir/src/cam.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/curve.c" "CMakeFiles/cglm.dir/src/curve.c.o" "gcc" "CMakeFiles/cglm.dir/src/curve.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/ease.c" "CMakeFiles/cglm.dir/src/ease.c.o" "gcc" "CMakeFiles/cglm.dir/src/ease.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/euler.c" "CMakeFiles/cglm.dir/src/euler.c.o" "gcc" "CMakeFiles/cglm.dir/src/euler.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/frustum.c" "CMakeFiles/cglm.dir/src/frustum.c.o" "gcc" "CMakeFiles/cglm.dir/src/frustum.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/io.c" "CMakeFiles/cglm.dir/src/io.c.o" "gcc" "CMakeFiles/cglm.dir/src/io.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/mat2.c" "CMakeFiles/cglm.dir/src/mat2.c.o" "gcc" "CMakeFiles/cglm.dir/src/mat2.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/mat3.c" "CMakeFiles/cglm.dir/src/mat3.c.o" "gcc" "CMakeFiles/cglm.dir/src/mat3.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/mat4.c" "CMakeFiles/cglm.dir/src/mat4.c.o" "gcc" "CMakeFiles/cglm.dir/src/mat4.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/plane.c" "CMakeFiles/cglm.dir/src/plane.c.o" "gcc" "CMakeFiles/cglm.dir/src/plane.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/project.c" "CMakeFiles/cglm.dir/src/project.c.o" "gcc" "CMakeFiles/cglm.dir/src/project.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/quat.c" "CMakeFiles/cglm.dir/src/quat.c.o" "gcc" "CMakeFiles/cglm.dir/src/quat.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/ray.c" "CMakeFiles/cglm.dir/src/ray.c.o" "gcc" "CMakeFiles/cglm.dir/src/ray.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/sphere.c" "CMakeFiles/cglm.dir/src/sphere.c.o" "gcc" "CMakeFiles/cglm.dir/src/sphere.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/vec2.c" "CMakeFiles/cglm.dir/src/vec2.c.o" "gcc" "CMakeFiles/cglm.dir/src/vec2.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/vec3.c" "CMakeFiles/cglm.dir/src/vec3.c.o" "gcc" "CMakeFiles/cglm.dir/src/vec3.c.o.d"
+  "/Users/andrewkuang/Downloads/☕Other🌱/CplusCraft/lib/cglm/src/vec4.c" "CMakeFiles/cglm.dir/src/vec4.c.o" "gcc" "CMakeFiles/cglm.dir/src/vec4.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
